238de7bfb4f9979c63ed76e39879b2b30a6f587d
Piyush Jain <piyushjain5350@gmail.com>
2023-09-07 01:15:54 +0530

    1 soln

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useDispatch ,useSelector} from 'react-redux';
+import { increment,decrement } from "../redux/action/counterAction";
 
 const App = () => {
+  const val=useSelector(state=>state.count);
+  // console.log(val)
+  const dispatch=useDispatch();
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>{val}</p>
+      <button onClick={()=>dispatch(increment())}>Increment</button>
+      <button onClick={()=>dispatch(decrement())}>Decrement</button>
     </div>
-  )
+  )   
 }
 
 export default App
